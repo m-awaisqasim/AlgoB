9a022ce921afccc3f8352cc3da6fc564204d14a8
Muhammad Awais <muhammadawais22969@gmail.com>
2026-04-16 19:04:57 +0500

    Start Assignment 4 (Last one)

--- From Assignment 3/main.cpp
+++ From Assignment 3/main.cpp
@@ -4,26 +4,20 @@ using namespace std;
 #include "Module_D_Awais_Part3.cpp"
 
 int main() {
-    string cyan = "\033[36m"; 
-    string gold = "\033[33m"; 
-    string reset = "\033[0m"; 
-
-    cout << cyan << endl;
     system("chcp 65001 > nul");
-    cout << "\t\t    ████╗  ██╗                 █████╗                    " << endl;
-    cout << "\t\t   ██╔═██╗ ██║  ████╗   ████╗  ██╔══██╗                   " << endl;
-    cout << "\t\t   ██████║ ██║ ██╔═██╗ ██╔═██╗ ██████╔╝                   " << endl;
-    cout << "\t\t   ██╔═██║ ██║ ██║ ██║ ██║ ██║ ██╔══██╗                   " << endl;
-    cout << "\t\t   ██║ ██║ ██║ ╚█████║ ╚████╔╝ ██████╔╝                   " << endl;
-    cout << "\t\t   ╚═╝ ╚═╝ ╚═╝  ╚══██║  ╚═══╝  ╚═════╝                    " << endl;
-    cout << "\t\t                 ████║                                    " << endl;
-    cout << "\t\t                 ╚═══╝                                   " << endl;
-    cout << "\t\t" << gold << "      ALGORITHMIC BACKTESTER (V3.0) " << cyan << "" << endl;
-    cout << reset << endl;
+    cout << "\t\t    ████╗  ██╗                 █████╗      " << endl;
+    cout << "\t\t   ██╔═██╗ ██║  ████╗   ████╗  ██╔══██╗    " << endl;
+    cout << "\t\t   ██████║ ██║ ██╔═██╗ ██╔═██╗ ██████╔╝    " << endl;
+    cout << "\t\t   ██╔═██║ ██║ ██║ ██║ ██║ ██║ ██╔══██╗    " << endl;
+    cout << "\t\t   ██║ ██║ ██║ ╚█████║ ╚████╔╝ ██████╔╝    " << endl;
+    cout << "\t\t   ╚═╝ ╚═╝ ╚═╝  ╚══██║  ╚═══╝  ╚═════╝     " << endl;
+    cout << "\t\t                 ████║                     " << endl;
+    cout << "\t\t                 ╚═══╝                     " << endl;
 
     receiveData();
     displaySummary();
     drawEquityCurve();
+    showVisualChart();
     startChatbot();
 
     cout << "\nProgram terminated. Goodbye, Have a Nice Trading Journey!" << endl;
